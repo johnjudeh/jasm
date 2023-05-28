38aec8ee7274519f801cb50b0537d7a24ccc52fe
John Judeh <johnjudeh@hotmail.com>
2023-05-28 17:58:15 +0100

    feat: Add DTA instruction type and ignore text at the end of a line

--- jasm.py
+++ jasm.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
-from constants import OPCODES, INSTRUCTIONS
+from constants import (
+    OPCODES,
+    INSTRUCTIONS,
+    PSEUDO_INSTRUCTION_DATA,
+    ASSEMBLED_LINE_TYPE,
+    ASSEMBLED_LINE_TYPE_DATA,
+    ASSEMBLED_LINE_TYPE_INSTRUCTION,
+    ASSEMBLED_LINE_ADDRESS,
+    ASSEMBLED_LINE_VALUE,
+    ASSEMBLED_LINE_OPCODE,
+    ASSEMBLED_LINE_OPERAND,
+    ASSEMBLED_LINE_INSTRUCTION_TEXT,
+)
 from cli import create_cli
 
 
@@ -12,30 +24,53 @@ def assemble(file_contents: str) -> None:
             continue
 
         line_iterator = iter(line)
-        assembled = []
+        assembled_line = {}
 
         instruction = next(line_iterator).lower()
-        if not instruction in INSTRUCTIONS:
+
+        if instruction == PSEUDO_INSTRUCTION_DATA:
+            assembled_line[ASSEMBLED_LINE_TYPE] = ASSEMBLED_LINE_TYPE_DATA
+
+            address = next(line_iterator)
+            if not address.isdigit():
+                raise ValueError(f"Non numeric address specified: {address}")
+            assembled_line[ASSEMBLED_LINE_ADDRESS] = int(address)
+
+            value = next(line_iterator)
+            if not value.isdigit():
+                raise ValueError(f"Non numeric value specified: {value}")
+            assembled_line[ASSEMBLED_LINE_VALUE] = int(value)
+
+        elif instruction in INSTRUCTIONS:
+            assembled_line[ASSEMBLED_LINE_TYPE] = ASSEMBLED_LINE_TYPE_INSTRUCTION
+            assembled_line[ASSEMBLED_LINE_OPCODE] = OPCODES[instruction]
+
+            try:
+                operand = next(line_iterator)
+            except StopIteration:
+                pass
+            else:
+                if not operand.isdigit():
+                    raise ValueError(f"Non numberic operand specified: {operand}")
+                assembled_line[ASSEMBLED_LINE_OPERAND] = int(operand)
+        else:
             raise ValueError(f"Invalid instruction specified: {instruction}")
-        assembled.append(OPCODES[instruction])
 
-        for operand in line_iterator:
-            if not operand.isnumeric():
-                raise ValueError(f"Non numberic operand specified: {operand}")
-            assembled.append(int(operand))
-
-        # Add the assembly instruction to map to the assembled code in output
-        assembled.append(" ".join(line))
-        assembled_lines.append(assembled)
+        assembled_line[ASSEMBLED_LINE_INSTRUCTION_TEXT] = " ".join(line)
+        assembled_lines.append(assembled_line)
 
     print("ADDRESS  INSTRUCTION  ASSEMBLY")
-    for address, assembled_line in enumerate(assembled_lines):
-        instruction_text = assembled_line.pop()
-        instruction = assembled_line[0]
-        operand = assembled_line[1] if len(assembled_line) > 1 else 0
 
+    for l, line in enumerate(assembled_lines):
+        if line[ASSEMBLED_LINE_TYPE] == ASSEMBLED_LINE_TYPE_DATA:
+            address = line[ASSEMBLED_LINE_ADDRESS]
+            instruction = f"{line[ASSEMBLED_LINE_VALUE]:08b}"
+            instruction = f"{instruction[:4]} {instruction[4:]}"
+        else:
+            address = l
+            instruction = f"{line[ASSEMBLED_LINE_OPCODE]:04b} {line.get(ASSEMBLED_LINE_OPERAND, 0):04b}"
         print(
-            f"{address:04b}     {instruction:04b} {operand:04b}    {instruction_text}"
+            f"{address:04b}     {instruction}    {line[ASSEMBLED_LINE_INSTRUCTION_TEXT]}"
         )
 
 
